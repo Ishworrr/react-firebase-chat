48062fa0133c654354b4fb3f5c3cc62fb3f6abec
Ishwor <ishwo5678@gmail.com>
2024-08-10 22:32:48 +1000

    7-1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import List from "./components/list/List";
+import Detail from "./components/details/Detail";
+
+import Chat from "./components/chat/chat";
+
+import ChatList from "./components/chatList/ChatList";
+import Userinfo from "./components/list/Userinfo/Userinfo";
+
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div className="container">
+      <List />
+      <Chat />
+      <Detail />
+      <ChatList />
+      <Userinfo />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/list/Userinfo/Userinfo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Userinfo = () => {
+  return (
+    <div className="userInfo">
+      userInfo
+      <div className="users">
+        <img src="./avatar.png" alt="" />
+        <h2>John</h2>
+      </div>
+      <div className="icons">
+        <img src="./more.png" alt="" />
+        <img src="./video.png" alt="" />
+        <img src="./edit.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Userinfo;
